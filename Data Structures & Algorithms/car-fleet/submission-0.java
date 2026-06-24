cc3f59a9b0e30657c7a696a5c034828be58bde8e
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-06-24 13:54:42 +0800

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        ArrayList<Car> cars = new ArrayList();
+        for (int i = 0; i < position.length; ++i) {
+            cars.add(new Car(position[i], speed[i]));
+        }
+        cars.sort(Comparator.comparing(Car::getPosition).reversed());
+        
+        ArrayDeque<Float> stack = new ArrayDeque();
+        for (Car car: cars) {
+            float t = (1f * target - car.position) / car.speed;
+            if (stack.isEmpty() || t > stack.peek()) {   // 追不上 → 新 fleet
+                stack.push(t);
+            }
+        }
+        return stack.size();
+    }
+
+    public class Car {
+        int position;
+        int speed;
+        Car(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
+        }
+
+        int getPosition() {
+            return position;
+        }
+    }
+}
