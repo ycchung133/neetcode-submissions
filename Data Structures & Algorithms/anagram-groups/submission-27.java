80574f3ae969d092516c24cc29c9b43cbce74a6a
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-06-25 10:02:29 +0800

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {    
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] a = new int[26];
+            for (char c : s.toCharArray()) {
+                a[c - 'a']++;
+            }
+            String key = Arrays.toString(a);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     Map<String, List<String>> map = new HashMap<>();
+    //     for (String s : strs) {
+    //         char[] chars = s.toCharArray();
+    //         Arrays.sort(chars);
+    //         String key = new String(chars);
+    //         if (!map.containsKey(key)) {
+    //             map.put(key, new ArrayList());
+    //         }
+    //         map.get(key).add(s);
+    //     }
+
+    //     return new ArrayList(map.values());
+    // }
+
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     Map<String, List<String>> h = new HashMap<>();
+    //     for (String s : strs) {
+    //         char[] chars = s.toCharArray();
+    //         Arrays.sort(chars);
+    //         String key = new String(chars);
+    //         if (!h.containsKey(key)) {
+    //             h.put(key, new ArrayList<String>());
+    //         }
+    //         h.get(key).add(s);
+    //     }
+        
+    //     return new ArrayList(h.values()); 
+    // }
+}
