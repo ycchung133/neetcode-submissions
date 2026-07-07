b9f4e6818a32b9e3d1985b65436d5d80d3824d65
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-07-07 14:00:39 +0800

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> temp = new ArrayList<>();
+        HashSet<List<Integer>> results = new HashSet<>();
+        temp.add(new ArrayList<>());
+        if (target == 0) {
+            results.add(new ArrayList<>());
+            return new ArrayList<>(results);
+        }
+        Arrays.sort(candidates);
+        for (int num : candidates) {
+            HashSet<List<Integer>> newTemp = new HashSet<>();
+            for (List<Integer> subset : temp) {
+                List<Integer> newSubset = new ArrayList<>(subset);
+                newSubset.add(num);
+                int sum = 0;
+                for (int j = 0; j < newSubset.size(); ++j) {
+                    sum += newSubset.get(j);
+                }
+                if (sum < target) {
+                    newTemp.add(newSubset);
+                } else if (sum == target) {
+                    newTemp.add(newSubset);
+                    results.add(newSubset);
+                }
+            }
+            for (List<Integer> subset : newTemp) {
+                temp.add(subset);
+            }
+        }
+        return new ArrayList<>(results);
+    }
+}
