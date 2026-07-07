07dd1658b32da1027d12702aa090bd6bc23739fb
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-07-07 15:12:08 +0800

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,49 @@
+class PrefixTree {
+
+    private TrieNode root = new TrieNode();
+
+    public PrefixTree() {
+
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];   // 26 個小寫字母的分岔
+        boolean isEnd = false;                     // 是不是雙圈(單字結尾)
+    }
+}
