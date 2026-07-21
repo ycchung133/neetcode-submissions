ddd91a5e099ada5c855a75656c05d007e8176f17
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-07-21 11:20:55 +0800

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int result = climbStairs(n - 1) + climbStairs(n - 2);
+        map.put(n, result);
+        return result;
+    }
+}
