41ca47688cb3103e88900eb74bb2aa5676598468
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-07-21 13:21:19 +0800

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] g = new int[n + 1];
+        g[0] = 0;
+        g[1] = nums[0];
+        if (n <= 1) {
+            return g[n];
+        }
+        for (int i = 2; i < g.length; ++i) {
+            g[i] = Math.max(g[i - 2] + nums[i - 1], g[i - 1]);
+        }
+        return g[n];
+    }
+
+    // public int rob(int[] nums) {
+    //     if (nums.length == 0) {
+    //         return 0;
+    //     } else if (nums.length == 1) {
+    //         return nums[0];
+    //     }
+    //     int[] g = new int[nums.length];
+    //     g[0] = nums[0];
+    //     g[1] = Math.max(nums[0], nums[1]);
+    //     for (int i = 2; i < nums.length; ++i) {
+    //         g[i] = Math.max(g[i - 1], g[i - 2] + nums[i]);
+    //     }
+    //     return g[nums.length - 1];
+    // }
+}
