c894b848568d86f79dde1c5c90eaba54d2e46250
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-08-28 10:03:00 +0800

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // Scan from left to right, calculate the product of all numbers on the left of current number.
+        int temp = 1;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0) {
+                temp *= nums[i - 1];
+            }
+            result[i] = temp;
+        }
+
+
+        // Scan from right to left, calculate the product of all numbers on the right of current number, then product with previous corresponding result.
+        temp = 1;
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (i < nums.length - 1) {
+                temp *= nums[i + 1];
+            }
+            result[i] *= temp;
+        }
+
+        return result;
+    }
+}  
