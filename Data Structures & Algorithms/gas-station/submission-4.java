814ee20e2b89ec6e961bbcf8bb814ddfb2c3cd22
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-09-22 14:32:23 +0800

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalTank = 0;
+        int tank = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; ++i) {
+            int diff = gas[i] - cost[i];
+            totalTank += diff;
+            tank += diff;
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+        return totalTank < 0 ? -1 : start;
+    }
+}
