c148c141bc11f672e47bad5c88f3ca9319c43617
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-09-24 11:43:56 +0800

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int[] interval : intervals) {
+            map.put(interval[0], Math.max(interval[1], map.getOrDefault(interval[0], 0)));
+        }
+        
+        List<int[]> results = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int start = entry.getKey();
+            int end = entry.getValue();
+            if (!results.isEmpty() && start <= results.get(results.size() - 1)[1]) {
+                int last[] = results.get(results.size() - 1);
+                last[1] = Math.max(last[1], end);
+            } else {
+                results.add(new int[] {start, end});
+            }
+        }
+        return results.toArray(new int[results.size()][]);
+    }
+}
