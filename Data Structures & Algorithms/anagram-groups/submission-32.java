2faac068fda9966c986f27c4230b03329a91feab
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-09-26 18:18:56 +0800

    Add: anagram-groups - submission-32

---
+++ Data Structures & Algorithms/anagram-groups/submission-32.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
