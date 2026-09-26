f61137731c8445e01caec4bb2ec01588e2a11990
ycchung133 <153521090+ycchung133@users.noreply.github.com>
2026-09-26 17:48:23 +0800

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+        return map.isEmpty();
+    }
+}
